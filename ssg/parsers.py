863306f84dd0523912afafdf746ad6a1d1aeba50
Vinicius Goncalves de Souza <vinicius@MacBook-Air-de-Vinicius.local>
2020-09-24 15:36:08 -0400

    correction

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -40,7 +40,7 @@ class MarkdownParser(Parser):
 
     def parse(self, path, source, dest):
         content = Content.load(self.read(path))
-        html = Markdown(content.body)
+        html = markdown(content.body)
         self.write(path, dest, html)
         sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
 
